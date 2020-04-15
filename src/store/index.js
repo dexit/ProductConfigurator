5f469328dffc16ef2dc676710a7424053963b947
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2020-04-15 10:08:46 +0200

    Install Vuex plugin

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import configurator from './modules/configurator'
-import account from './modules/account'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import configurator from './modules/configurator';
+import account from './modules/account';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-    configurator,
-    account
-  }
-})
+    modules: {
+        configurator,
+        account,
+    },
+});
